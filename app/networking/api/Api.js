df220a6ccf9fbf80201408498c1c642068818458
kinjatoro <lucaslopezvazquez1@gmail.com>
2024-02-09 20:19:06 -0300

    react-native-config, axios y env

---
+++ app/networking/api/Api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import Config from 'react-native-config';
+
+axios.defaults.baseURL = Config.BASE_URL;
+axios.defaults.timeout = Config.TIME_OUT;
+axios.defaults.headers.common = {
+Accept: 'application/json' , // el formato que espero que la info vuelva
+'Content-Type' : 'application/json' ,// el formato en que le mando la info
+};
+function setClientToken (token) {
+axios.defaults.headers.common = {Authorization: 'bearer ' + {token}};
+}
+export default axios;
---
+++ app/networking/api/endpoints/loginWS.js
@@ -0,0 +1,20 @@
+import api from '../api';
+export default loginWS = {
+login: async function (mail, password, deviceToken ) {
+ return await api.post('/login', {
+ mail,
+ password,
+ token: deviceToken , //puedo especificar la key
+ });
+ },
+ deleteNode : async function (nodeId) {
+ return await api.delete(`/nodes/${nodeId}`);
+ },
+}
+
+//optional parameters
+const params = {}
+if (this.mail) { params.mail = this.mail }
+return await api.post('/login', {
+ params
+ });
